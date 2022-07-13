9b881444a527ac9762f92bc5dc0379b24c77840c
kyrie <13148656108@163.com>
2022-07-13 22:24:32 +0800

    feat

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+// npm link 绑定 bin
+console.log("kyrie cli")
+
+const program = require('commander')
+const helpOptions = require('./lib/core/help')
+const createCommands = require('./lib/core/create')
+
+//查看版本号
+program.version(require('./package.json').version)
+
+//帮助和可选信息
+helpOptions()
+
+//创建指令
+createCommands()
+
+program.parse(process.argv)
